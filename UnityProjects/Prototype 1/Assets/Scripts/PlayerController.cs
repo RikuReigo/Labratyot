73457ad782d740efd5e40913b22c56183c2c3237
Riku Juvonen <reigos12@gmail.com>
2020-04-17 11:19:43 +0300

    Prototype 2
    Unity prototype 2 lisätty

--- UnityProjects/Prototype 1/Assets/Scripts/PlayerController.cs
+++ UnityProjects/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 17;
+    public float turnSpeed = 15;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        //transform.Translate(0,0,1);
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //transform.Translate(0,0,1); test code piece
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); //this piece of code moves vehicle forward
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); //this piece of code rotates vehicle
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput); test code piece
     }
 }
---
+++ UnityProjects/Prototype 2/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+    private int ballIndex;
+    private float startDelay = 1f;
+    private float spawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        //Randomise Interval
+        spawnInterval = Random.Range(2f, 4f);
+
+        //Randomise ball
+        ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        // Repeat the progress
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+
+}
